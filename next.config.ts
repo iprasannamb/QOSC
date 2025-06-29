5aabd379042946a7ffadbb7dcfaf265519a4f434
Djnihaal <odathernihaal@gmail.com>
2025-06-30 00:35:37 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
-<<<<<<< HEAD
 
-=======
->>>>>>> 57a263e8ad3c3bc29308e3767c3878f1986c08bf
 export default nextConfig;
